52e4c5605affed10d924de930c6f92fe98896966
ttaerrim <ltr0121@naver.com>
2022-01-24 21:07:11 +0900

    Add: 수취 국가 선택 시 환율 출력

--- src/Components/FirstCalc.jsx
+++ src/Components/FirstCalc.jsx
@@ -4,7 +4,8 @@ import style from "./FirstCalc.module.css";
 const FirstCalc = () => {
   const access_key = "4e8b4a82ea5e0ae977ad9426c136f301";
   const [quotes, setQuotes] = useState([]);
-  const [exchangeRate, setExchangeRate] = useState(1);
+  const [exchangeRate, setExchangeRate] = useState(1195.755008);
+  const [country, setCountry] = useState("KRW");
 
   const getCurrency = async () => {
     const response = await fetch(
@@ -14,7 +15,9 @@ const FirstCalc = () => {
     setQuotes(json.quotes);
   };
 
-  useEffect(getCurrency, [quotes]);
+  useEffect(() => {
+    getCurrency();
+  }, []);
 
   //환율 계산하는 함수
   //달러 => 원화 달러금액 X 환율 = 수취금액
@@ -22,19 +25,16 @@ const FirstCalc = () => {
   const handleChangeCountry = (event) => {
     switch (event.target.value) {
       case "krw":
-        console.log(quotes.USDKRW);
-        // setExchangeRate(quotes.USDKRW);
+        setExchangeRate(quotes.USDKRW);
         break;
       case "jpy":
-        console.log(quotes);
-        // setExchangeRate(quotes.USDJPY);
+        setExchangeRate(quotes.USDJPY);
         break;
       case "php":
-        console.log(quotes);
-        // setExchangeRate(quotes.USDPHP);
+        setExchangeRate(quotes.USDPHP);
         break;
     }
-    console.log(exchangeRate);
+    setCountry(event.target.value.toUpperCase());
   };
 
   return (
@@ -42,9 +42,9 @@ const FirstCalc = () => {
       <h1>환율 계산</h1>
       <div className={`${style.FirstCalc}`}>
         <div>
-          <p>송금국가 : 미국 (USD)</p>
+          <p>송금국가: 미국 (USD)</p>
           <div className={`${style.receive}`}>
-            <p>수취국가 : </p>
+            <p>수취국가: </p>
             <select onChange={handleChangeCountry}>
               <option value="krw">한국(KRW)</option>
               <option value="jpy">일본(JPY)</option>
@@ -53,11 +53,13 @@ const FirstCalc = () => {
           </div>
 
           <div className={`${style.receive}`}>
-            <p>환율 :</p>
+            <p>
+              환율: {exchangeRate} {country}/USD
+            </p>
           </div>
 
           <div className={`${style.receive}`}>
-            <p>송금액 : </p>
+            <p>송금액: </p>
             <input />
             <p>USD</p>
           </div>
