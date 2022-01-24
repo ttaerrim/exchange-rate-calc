d7dc33b9f4c2152d3b44b70ad067dc28175c43d7
Ubermensch0608 <melancholyee28@gmail.com>
2022-01-25 00:50:58 +0900

    Add: 환율API fetch 동작 완료

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -4,6 +4,24 @@ import InputBar from "./InputBar";
 import Tab from "./Tab";
 
 function Calc() {
+  async function fetchCurrency() {
+    const response = await fetch(
+      "http://api.currencylayer.com/live?access_key=e0e5c1c6b9fb3dee1c72593e85deb3b2&format=1"
+    );
+
+    if (!response.ok) {
+      const message = `다음과 같은 에러가 발생했습니다: ${response.status}`;
+      throw new Error(message);
+    }
+
+    const currency = await response.json();
+    return currency;
+  }
+
+  fetchCurrency().then((currency) => {
+    console.log(currency["quotes"]);
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.calcBox}>
