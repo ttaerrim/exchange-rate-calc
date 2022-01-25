b8f0f2cc19ba35a147b1a6c0725bb215404731c9
ttaerrim <ltr0121@naver.com>
2022-01-26 00:43:26 +0900

    Refactor: 불필요한 코드 제거

--- src/components/FirstCalc.jsx
+++ src/components/FirstCalc.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import styles from "./FirstCalc.module.css";
 
