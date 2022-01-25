1c01a5e02c9760842f6a84216f95fc45278f0f91
ttaerrim <ltr0121@naver.com>
2022-01-25 17:18:34 +0900

    Modify: 금액 소수점 둘째 자리 출력 & useEffect 의존성 배열 수정

--- src/Components/FirstCalc.jsx
+++ src/Components/FirstCalc.jsx
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from "react";
 import style from "./FirstCalc.module.css";
 
 const FirstCalc = () => {
-  const access_key = "4e8b4a82ea5e0ae977ad9426c136f301";
+  const apiKey = "e0e5c1c6b9fb3dee1c72593e85deb3b2";
   const [quotes, setQuotes] = useState([]);
-  const [exchangeRate, setExchangeRate] = useState(1195.755008);
-  const [country, setCountry] = useState("KRW");
+  const [exchangeRate, setExchangeRate] = useState(1);
+  const [nation, setNation] = useState("KRW");
+  //아래 state 하나로 합치기 ->리팩토링
+  const [sendMoney, setSendMoney] = useState(0);
+  const [getMoney, setGetMoney] = useState(0);
+
+  const [isShow, setIsShow] = useState(false);
 
   const getCurrency = async () => {
     const response = await fetch(
-      "http://api.currencylayer.com/live?access_key=" + access_key
+      `http://api.currencylayer.com/live?access_key=${apiKey}&format=1`
     );
     const json = await response.json();
     setQuotes(json.quotes);
@@ -17,10 +22,7 @@ const FirstCalc = () => {
 
   useEffect(() => {
     getCurrency();
-  }, []);
-
-  //환율 계산하는 함수
-  //달러 => 원화 달러금액 X 환율 = 수취금액
+  }, [quotes.USDKRW, quotes.USDJPY, quotes.USDPHP]);
 
   const handleChangeCountry = (event) => {
     switch (event.target.value) {
@@ -33,44 +35,69 @@ const FirstCalc = () => {
       case "php":
         setExchangeRate(quotes.USDPHP);
         break;
+      default:
+        break;
     }
-    setCountry(event.target.value.toUpperCase());
+    setIsShow(false);
+    setNation(event.target.value.toUpperCase());
+  };
+
+  const handleSendMoney = (e) => {
+    setSendMoney(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setGetMoney((curr) =>
+      (sendMoney * exchangeRate).toLocaleString(undefined, {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+      })
+    );
+    setIsShow(true);
   };
 
   return (
     <>
       <h1>환율 계산</h1>
-      <div className={`${style.FirstCalc}`}>
+      <div>
         <div>
-          <p>송금국가: 미국 (USD)</p>
-          <div className={`${style.receive}`}>
-            <p>수취국가: </p>
+          <p>송금국가: 미국(USD)</p>
+          <div>
+            수취국가:
             <select onChange={handleChangeCountry}>
-              <option value="krw">한국(KRW)</option>
+              <option value="krw" select="true">
+                한국(KRW)
+              </option>
               <option value="jpy">일본(JPY)</option>
               <option value="php">필리핀(PHP)</option>
             </select>
           </div>
 
-          <div className={`${style.receive}`}>
+          <div>
             <p>
-              환율: {exchangeRate} {country}/USD
+              환율 :
+              {exchangeRate.toLocaleString(undefined, {
+                maximumFractionDigits: 2,
+                minimumFractionDigits: 2,
+              })}{" "}
+              {nation}/USD
             </p>
           </div>
 
-          <div className={`${style.receive}`}>
-            <p>송금액: </p>
-            <input />
-            <p>USD</p>
+          <div>
+            <p>
+              송금액:
+              <input value={sendMoney} onChange={handleSendMoney} />
+              USD
+            </p>
           </div>
-
-          <button>Submit</button>
+          <button onClick={handleSubmit}>Submit</button>
         </div>
       </div>
-      <div>
-        <p>수취금액은 :</p>
-        {}
-        <p>입니다.</p>
+      <div style={isShow ? { display: "inline-block" } : { display: "none" }}>
+        <p>
+          수취금액은 {getMoney} {nation} 입니다.
+        </p>
       </div>
     </>
   );
